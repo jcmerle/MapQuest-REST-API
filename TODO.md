4233fa480f19e5fa14be1df53eae7bd039012af3
ekiholic <cedric.cheng@epitech.eu>
2023-04-24 07:43:21 +0000

    Added more map features

--- TODO.md
+++ TODO.md
@@ -2,7 +2,8 @@
 Application that retrieves JSON data from the MapQuest Directions API, parses the data, and formats it for output to the user.
 
 ### Work in progress
-- [ ] Task name - student ID
 
 ### Completed ✓
-- [✓] Completed task title - student ID
+- [✓] Added Star & End location on map - 12224918
+- [✓] Added traffic and construction on map - 12224918
+- [✓] Different display according to number of locations - 12224918
--- mapquest.py
+++ mapquest.py
@@ -1,8 +1,20 @@
 import urllib.parse
 import requests
+from PIL import Image
+from io import BytesIO
 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
+static_map_api = "https://www.mapquestapi.com/staticmap/v5/map?"
 key = "3vVAToG1jRZVLBtLMDdyciN3z2jmpgen"
+map_options = ["map", "dark", "light", "hyb", "sat"]
+
+# Display map function
+def display_map(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.show()
+    return response
+
 
 # Loop to get user inputs and fetch directions
 while True:
@@ -33,7 +45,41 @@ while True:
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
 
+    # Prepare map request
+    map_type = input("Enter map type (map, hyb, sat, light, dark): ")
+    if map_type == "q" or map_type == "quit":
+        break
+    elif map_type == "":
+        map_type = "map"
+    while map_type not in map_options:
+        map_type = input("Invalid map type, try again : ")
+    # Check if there are multiple locations
+    if len(dest_list) > 1:
+        static_map_params = {
+            "key": key,
+            "size": "600,400@2x",
+            "locations":orig + '||' + '||'.join(map(str,dest_list)),
+            "defaultMarker":"marker-num",
+            "type": map_type,
+            "scalebar":"true",
+            "traffic":"flow|cons",
+        }
+    else:
+        static_map_params = {
+            "key": key,
+            "size": "600,400@2x",
+            "start": orig + "|flag-start",
+            "end": dest_list[-1] + "|flag-end",
+            "type": map_type,
+            "scalebar":"true",
+            "traffic":"flow|cons",
+        }
+    # Construct the API request URL
+    static_map_url = static_map_api + urllib.parse.urlencode(static_map_params)
+    print("Static Map URL: " + static_map_url)
 
+    # Display map
+    map_status = display_map(static_map_url)
 
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
@@ -57,6 +103,7 @@ while True:
         # Print the total trip duration and distance    
         print("Total Trip Duration: " + "{:02d}:{:02d}:{:02d}".format(total_duration // 3600, (total_duration % 3600) // 60, total_duration % 60))
         print("Total Trip Kilometers: " + str("{:.2f}".format(total_distance))+ "\n")
+
     
     # Handle other status codes (errors) and print relevant messages        
     elif json_status == 402:
