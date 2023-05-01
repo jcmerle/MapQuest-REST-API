0ccb487c743dae3bcbe0577ca531b3fcac011d41
JC <98285475+jcmerle@users.noreply.github.com>
2023-05-01 12:20:43 +0900

    Delete test directory

--- mashallah/test.py
+++
@@ -1,8 +0,0 @@
-# Imports PIL module 
-from PIL import Image
-  
-# open method used to open different extension image file
-im = Image.open(r"bear.jpg") 
-  
-# This method will show image in any image viewer 
-im.show() 
